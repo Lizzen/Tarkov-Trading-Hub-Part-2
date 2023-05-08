752464c131490c3f4ae197dbc47b2d11ab4515c8
Lizzen <daviddfdiaz@gmail.com>
2023-05-08 22:40:42 +0200

    sidebar despegable (esta comentado)

--- includes/vistas/comun/sidebarIzq.php
+++ includes/vistas/comun/sidebarIzq.php
@@ -10,28 +10,35 @@ $app = Aplicacion::getInstance();
 
 </head>
 
-<div class="nav-wrapper">
+<div class="nav-wrapper" id="sidebar">
 	<div class="nav-menu">
+
+		<div class="elem-caja">
+			<div class="elem-imagen">
+				<!--<img src="./css/img/sidebar.png" id="toggleSummaries" alt="inventario" class="icon-small">-->
+			</div>
+		</div>
 
 		<div class="elem-caja">
 			<div class="elem-imagen">
 				<img src="./css/img/inventario_dorado.png" alt="inventario" class="icon-small" onclick="window.location.href='inventario.php'">
 			</div>
 			<div class="elem-details">
-				<a aria-current="page" href="./inventario.php" class="a-inventory">Inventario</a>
+					<a aria-current="page" href="./inventario.php" class="a-inventory" id="inventario">Inventario</a>
 			</div>
 		</div>
 
 		<div class="elem-caja">
 			<div class="elem-imagen">
-				<img src="./css/img/mercado_mini.png" alt="mercado" class="icon-small" onclick="window.location.href='mercado.php'">
+				<img src="./css/img/mercado_mini.png" alt="mercado" class="icon-small" onclick="window.location.href='mercado.php?id=inicio'">
 			</div>
 			<div class="elem-details">
 				<details>
 					<summary>Mercado</summary>
-					<a href="./mercado.php" class="a-inventory">Inicio</a>
-					<a href="./mercado.php" class="a-inventory">Iniciar Venta</a>
-					<a href="./mercado.php" class="a-inventory">Mis Ventas</a>
+					<a href="./mercado.php?id=inicio" class="a-inventory">Inicio</a>
+					<a href="./mercado.php?id=compra" class="a-inventory">Comprar</a>
+					<a href="./mercado.php?id=venta" class="a-inventory">Vender</a>
+					<a href="./mercado.php?id=mis_ventas" class="a-inventory">Mis Ventas</a>
 				</details>
 			</div>
 		</div>
